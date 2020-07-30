aa1105e1593b072e8800131b2b322c0c416cfa9e
PikaSama <pikasama@qq.com>
2020-07-30 20:47:04 +0800

    7.30 notes

---
+++ 7.30/booleanTest.java
@@ -0,0 +1,19 @@
+public class booleanTest {
+    public static void main(String[] args) {
+        boolean b1 = false;
+        boolean b2 = false;
+        //?????
+        if(b1) {
+            System.out.println("b1=T");
+        }
+        else {
+            System.out.println("b1=F");
+        }
+        if(b2) {
+            System.out.println("b2=T");
+        }
+        else {
+            System.out.println("b2=F");
+        }
+    }
+}
---
+++ 7.30/helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld {
+    public static void main(String[] args) { 
+        String helloworld="hello world!";
+        System.out.println(helloworld);
+    }
+}
