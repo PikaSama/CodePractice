4d74863210d17d96be5056144178f44ca41e349d
PikaSama <pikasama@qq.com>
2021-01-15 12:37:01 +0800

    vue

---
+++ Vue/1.13/todolist/src/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="todolist">
+    <input v-model="inputValue" />
+    <span>{{ inputValue }}</span>
+    <button @click="addItem">Add</button>
+    <ul>
+      <TodoItem v-for="(item,index) in todoList" :key="index" :content="item" :index="index" @delete="deleteItem(index)"></TodoItem>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+
+export default {
+  components: { TodoItem },
+  data () {
+    return {
+      inputValue: '',
+      todoList: []
+    }
+  },
+  methods: {
+    addItem () {
+      this.todoList.push(this.inputValue)
+      this.inputValue = ''
+    },
+    deleteItem (index) {
+      this.todoList.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
