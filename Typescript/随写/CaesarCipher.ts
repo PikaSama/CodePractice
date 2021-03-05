5b7ccdf8402d16641ca26007e4c2948a38db2415
PikaSama <pikasama@qq.com>
2021-03-05 18:01:38 +0800

    update

--- Typescript/随写/CaesarCipher.ts
+++ Typescript/随写/CaesarCipher.ts
@@ -1,23 +1,18 @@
-const str = "DSKDVLDF VRXO";
+const str = 'DSKDVLDF VRXO';
 
-interface NumberList {
-    encrypt: number,
-    decrypt: number,
-}
-
-function CaesarCipher(action: string, str: string, offset: number): string {
-    const parsedOffset = ~~offset;
+function CaesarCipher(str: string, offset: number): string {
+    let parsedOffset = ~~offset;
     const strBox = [''];
-    const actions: Map<string, Function> = new Map([
-        ['encrypt',encrypt],
-        ['decrypt',decrypt],
-    ]);
+    if (parsedOffset >= 0) {
+        parsedOffset %= 26;
+    } else {
+        parsedOffset = 26 - (Math.abs(parsedOffset) % 26);
+    }
 
-    function encrypt(char: number, nums: NumberList): void {
-        const { encrypt: num } = nums;
+    function moveSym(char: number, num: number): void {
         // 122 - offset + 1
         // 90 - offset + 1
-        if (char >= (num - parsedOffset)) {
+        if (char >= num - parsedOffset) {
             // 97 + (offset - 1) - (122 - code)
             // 65 + (offset - 1) - (90 - code)
             strBox.push(String.fromCharCode(char - 26 + parsedOffset));
@@ -26,50 +21,28 @@ function CaesarCipher(action: string, str: string, offset: number): string {
         }
     }
 
-    function decrypt(char: number, nums: NumberList): void {
-        const { decrypt: num } = nums;
-        // 97 + offset - 1
-        // 65 + offset - 1
-        if (char <= (num + parsedOffset)) {
-            // 122 - (offset - 1) + (char - 97)
-            // 90 - (offset - 1) + (char - 65)
-            strBox.push(String.fromCharCode(char + 26 - parsedOffset));
-        } else {
-            strBox.push(String.fromCharCode(char - parsedOffset));
-        }
-    }
-
     for (const i of str) {
-        const code = i.charCodeAt(0);
-        const func = actions.get(action);
+        const sym = i.charCodeAt(0);
         switch (true) {
             // Lowercase letter
-            case code >= 97 && code <= 122: {
-                const list: NumberList = {
-                    encrypt: 123,
-                    decrypt: 96,
-                };
-
-                func.call(CaesarCipher, code, list);
+            case sym >= 97 && sym <= 122: {
+                moveSym(sym, 123);
                 break;
             }
             // Uppercase letter
-            case code >= 65 && code <= 90: {
-                const list: NumberList = {
-                    encrypt: 91,
-                    decrypt: 64,
-                }
-               func.call(CaesarCipher, code, list);
-               break;
+            case sym >= 65 && sym <= 90: {
+                moveSym(sym, 91);
+                break;
             }
             // Other symbols
             default: {
-                strBox.push(String.fromCharCode(code));
+                strBox.push(String.fromCharCode(sym));
                 break;
             }
         }
     }
+
     return strBox.join('');
 }
 
-console.log(CaesarCipher('decrypt', str, 3));
+console.log(CaesarCipher(str, -3));
--- Typescript/随写/dist/CaesarCipher.js
+++ Typescript/随写/dist/CaesarCipher.js
@@ -1,16 +1,17 @@
-const str = "DSKDVLDF VRXO";
-function CaesarCipher(action, str, offset) {
-    const parsedOffset = ~~offset;
+const str = 'DSKDVLDF VRXO';
+function CaesarCipher(str, offset) {
+    let parsedOffset = ~~offset;
     const strBox = [''];
-    const actions = new Map([
-        ['encrypt', encrypt],
-        ['decrypt', decrypt],
-    ]);
-    function encrypt(char, nums) {
-        const { encrypt: num } = nums;
+    if (parsedOffset >= 0) {
+        parsedOffset %= 26;
+    }
+    else {
+        parsedOffset = 26 - (Math.abs(parsedOffset) % 26);
+    }
+    function moveSym(char, num) {
         // 122 - offset + 1
         // 90 - offset + 1
-        if (char >= (num - parsedOffset)) {
+        if (char >= num - parsedOffset) {
             // 97 + (offset - 1) - (122 - code)
             // 65 + (offset - 1) - (90 - code)
             strBox.push(String.fromCharCode(char - 26 + parsedOffset));
@@ -19,48 +20,26 @@ function CaesarCipher(action, str, offset) {
             strBox.push(String.fromCharCode(char + parsedOffset));
         }
     }
-    function decrypt(char, nums) {
-        const { decrypt: num } = nums;
-        // 97 + offset - 1
-        // 65 + offset - 1
-        if (char <= (num + parsedOffset)) {
-            // 122 - (offset - 1) + (char - 97)
-            // 90 - (offset - 1) + (char - 65)
-            strBox.push(String.fromCharCode(char + 26 - parsedOffset));
-        }
-        else {
-            strBox.push(String.fromCharCode(char - parsedOffset));
-        }
-    }
     for (const i of str) {
-        const code = i.charCodeAt(0);
-        const func = actions.get(action);
+        const sym = i.charCodeAt(0);
         switch (true) {
             // Lowercase letter
-            case code >= 97 && code <= 122: {
-                const list = {
-                    encrypt: 123,
-                    decrypt: 96,
-                };
-                func.call(CaesarCipher, code, list);
+            case sym >= 97 && sym <= 122: {
+                moveSym(sym, 123);
                 break;
             }
             // Uppercase letter
-            case code >= 65 && code <= 90: {
-                const list = {
-                    encrypt: 91,
-                    decrypt: 64,
-                };
-                func.call(CaesarCipher, code, list);
+            case sym >= 65 && sym <= 90: {
+                moveSym(sym, 91);
                 break;
             }
             // Other symbols
             default: {
-                strBox.push(String.fromCharCode(code));
+                strBox.push(String.fromCharCode(sym));
                 break;
             }
         }
     }
     return strBox.join('');
 }
-console.log(CaesarCipher('decrypt', str, 3));
+console.log(CaesarCipher(str, -3));
